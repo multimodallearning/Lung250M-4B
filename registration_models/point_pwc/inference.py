06e7f30ff54cce890da2f16d7a93ece3897e41a8
Alexander Bigalke <bigalke@zoe.imi.uni-luebeck.de>
2023-06-14 13:44:27 +0200

    adjusted PPWC inference

--- registration_models/point_pwc/inference.py
+++ registration_models/point_pwc/inference.py
@@ -21,8 +21,8 @@ def main(args):
     model.to(device)
 
     # data
-    idx_8k = torch.load('all_ind_8192.pth', map_location='cpu')
-    cases = [2, 8, 54, 55, 56, 94, 97, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113]
+    cases = [2, 8, 54, 55, 56, 94, 97, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119,
+             120, 121, 122, 123]
     out_dict = {'predictions': [],
                 'case_list': cases,
                 'keypts_src': []}
@@ -31,12 +31,8 @@ def main(args):
     model.eval()
 
     for i, case in enumerate(cases):
-        pcd_tgt = torch.load(os.path.join(args.cloudfolder,'case_{:03d}_{}.pth'.format(case, 1)))[2].float()
-        pcd_src = torch.load(os.path.join(args.cloudfolder,'case_{:03d}_{}.pth'.format(case, 2)))[2].float()
-        idx_8k_tgt = idx_8k['all_ind_fix'][case]
-        idx_8k_src = idx_8k['all_ind_mov'][case]
-        pcd_tgt = pcd_tgt[idx_8k_tgt]
-        pcd_src = pcd_src[idx_8k_src]
+        pcd_tgt = torch.load(os.path.join(args.cloudfolder,'case_{:03d}_{}.pth'.format(case, 1)))[0].float()
+        pcd_src = torch.load(os.path.join(args.cloudfolder,'case_{:03d}_{}.pth'.format(case, 2)))[0].float()
         pcd_tgt = pcd_tgt.unsqueeze(0).to(device)
         pcd_src = pcd_src.unsqueeze(0).to(device)
 
@@ -71,10 +67,10 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='inference of PointPWC-Net on Lung250M-4B')
 
-    parser.add_argument('-M', '--model', default='ppwc_syn.pth', help="model file (pth)")
+    parser.add_argument('-M', '--model', default='ppwc_sup.pth', help="model file (pth)")
     parser.add_argument('-C', '--cloudfolder', default='cloudsTs',
                         help="folder containing (/case_???_{1,2}.nii.gz)")
-    parser.add_argument('-O', '--outfile', default='predictions.pth',
+    parser.add_argument('-O', '--outfile', default='predictions_sup.pth',
                         help="output file for keypoint displacement predictions")
     parser.add_argument('--config', default='config_ppwc_sup.yaml',
                         help="config file of the model (yaml)")
