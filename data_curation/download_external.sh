c86cb3cefa0a5fb695cfd455450763f3c840bf06
mattiaspaul <mattiaspaul@users.noreply.github.com>
2024-01-25 18:35:49 +0100

    Add files via upload

---
+++ data_curation/download_external.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+gdown https://drive.google.com/u/0/uc?id=10NDcSlhpj9xFR4YecWLVh6l1p4s3kA_j&export=download
+gdown https://drive.google.com/u/0/uc?id=1SmgcZqM08KbMIvAzW0oFdh2Yqbje2NHy&export=download
+gdown https://drive.google.com/u/0/uc?id=1chdutKAJSENlztKo1IrzBcDcvuJFBHit&export=download
+gdown https://drive.google.com/u/0/uc?id=17B4zrTHepyN7hkYWOR9aYwndYhpN42Ei&export=download
+gdown https://drive.google.com/u/0/uc?id=1JCM_GkWNUR6B-gySmtn5T4r8KIGWn6sR&export=download
+unzip scans01to06.zip
+unzip scans07to13.zip
+unzip scans14to20.zip
+unzip scans21to25.zip
+unzip scans26to30.zip
